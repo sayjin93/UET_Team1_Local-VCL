9f2adbb3d9deafa232ed2b94ca549a48de2ad64f
Jurgen Kruja <jurgen-kruja@live.com>
2024-05-28 04:35:21 +0200

    Empty check for posts in profile page

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -2,6 +2,10 @@ import "./list.scss";
 import Card from "../card/Card";
 
 function List({ posts }) {
+  if (posts.length === 0) {
+    return <p>Empty list</p>;
+  }
+
   return (
     <div className="list">
       {posts.map((item) => (
